c6df81ccf1d1af7fa7fd5eef54b8ebb5d0791783
Sinabdeev Alexei <130910572+Alex-Sinabdeev@users.noreply.github.com>
2023-04-27 12:41:10 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,161 @@
+<?php
+$info = [
+    [
+        'work_name' => 'Водитель',
+        'work_date' => 'Сентябрь 2020 - Май 2020',
+        'work_descrip' => 'Был профессиональым водителем Президента ассоциации настольного тениса',
+    ],
+    [
+        'work_name' => 'Секретарь',
+        'work_date' => 'Май 2020 - Ноябрь 2021',
+        'work_descrip' => 'Работал на должности секретаря в МФЦ',
+    ],
+    [
+        'work_name' => 'Скрипач',
+        'work_date' => 'Декабрь 2022 - текущий',
+        'work_descrip' => 'Лучший скрипач по версии Форбс 2022',
+    ],
+];
+
+?>
+<!DOCTYPE html>
+<html>
+<руководитель>
+  >title<Мое резюме</title>
+  кодировкаmeta <="UTF-8">
+  namemeta <="окно просмотра" content="width=ширинаустройства, начальный масштаб=1">
+  относительнаяссылка <="таблица стилей" href="https://www.w3schools.com/w3css/4/w3.css ">
+  относительнаяссылка <="предварительно подключить" href="https://fonts.googleapis.com ">
+  относительнаяссылка <="предварительно подключить" href="https://fonts.gstatic.com " crossorigin>
+  hrefссылка <="https://fonts.googleapis.com/css2?family=Jost:wght@300&display=swap " rel="таблица стилей">
+  относительнаяссылка <="таблица стилей" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css ">
+  <стиль>
+    HTML,
+    Тело,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6 {
+      ,'Jost' :семействошрифтов без засечек;
+    }
+  </стиль>
+</глава>
+телокласса <="w3-светло-серый">
+  <!-- Контейнер страницы -->
+  классdiv <="w3-содержимое w3-margin-top" style="максимальная ширина: 1400 пикселей;">
+    <!-- Сетка -->
+    классdiv <="w3-row-padding">
+      <!-- Левая колонка -->
+      классdiv <="w3-третий">
+        классdiv <="w3-белый w3-текст-серый w3-карточка-4">
+          классdiv <="w3-display-container">
+            srcimg <="https://imgfon.ru/Images/Details_img_880px/Animals/glaza-belyy_fon-kotik-morda-polosatyy.webp"
+              style="ширина:100%" alt="Аватар">
+            классdiv <="w3-дисплей-слева внизу w3-контейнер w3-текст-черный">
+              >h2<Имя</h2>
+            </div>
+          </div>
+          классdiv <="w3-контейнер">
+            классi<>p <="fa fa-портфель fa-fw w3-правое поле w3-большой w3-текст-бирюзовый"></i>Профессия</p>
+            classi<>p <="fa fa-home fa-fw w3-margin-right w3-large w3-text-teal"></i>Город, страна</p>
+            классi<>p <="fa fa-конверт fa-fw w3-правое поле w3-большой w3-текст-бирюзовый"></i>Электроннаяпочта</p>
+            классi<>p <="fa fa-phone fa-fw w3-margin-right w3-large w3-text-teal"></i>Телефон</p>
+ 
+            <hr>
+ 
+            классp <="w3-large"><b><i class="fa fa-звездочка fa-fw w3-правое поле w3-текст-бирюзовый"></i>Навыки</b></p >
+            >p<Adobe Photoshop</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge w3-маленький">
+              классdiv <="w3-контейнер w3-центр w3-круглый-xlarge w3-бирюзовый" style="ширина: 90%">90%</div>
+            </div>
+            >p<Фотография</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge w3-маленький">
+              классdiv <="w3-контейнер w3-центр w3-круглый-xlarge w3-бирюзовый" style="ширина: 80%">
+                классdiv <="w3-center w3-text-white">80%</div>
+              </div>
+            </div>
+            >p<Иллюстратор</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge w3-маленький">
+              классdiv <="w3-контейнер w3-центр w3-круглый-xlarge w3-бирюзовый" style="ширина: 75%">75%</div>
+            </div>
+            >p<Медиа</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge w3-маленький">
+              классdiv <="w3-контейнер w3-центр w3-круглый-xlarge w3-бирюзовый" style="ширина: 50%">50%</div>
+            </div>
+            <br>
+ 
+            классp <="w3-large w3-text-theme"><b><i class="fa fa-globe fa-fw w3-margin-right w3-text-teal"></i>Языки</b>
+            </p>
+            >p<Английский</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge">
+              классdiv <="w3-round-xlarge w3-teal" style="высота: 24 пикселя; ширина: 100%"></div>
+            </div>
+            >p<Испанский</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge">
+              классdiv <="w3-round-xlarge w3-teal" style="высота: 24 пикселя; ширина: 55%"></div>
+            </div>
+            >p<Немецкий</p>
+            классdiv <="w3-светло-серый w3-круглый-xlarge">
+              классdiv <="w3-round-xlarge w3-teal" style="высота: 24 пикселя; ширина: 25%"></div>
+            </div>
+            <br>
+          </div>
+        </div>
+        <br>
+        <!-- End Left Column -->
+      </div>
+      <!-- Right Column -->
+      <div class="w3-twothird">
+        <div class="w3-container w3-card w3-white w3-margin-bottom">
+          <h2 class="w3-text-grey w3-padding-16"><i
+              class="fa fa-suitcase fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Опыт работы</h2>
+          <?php for ($i=0; $i < count($info); $i++):?>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b><?php echo $info[$i]['work_name']?></b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i><?php echo $info[$i]['work_date']?><span
+                                                                                                                                      class="w3-tag w3-teal w3-round"></span></h6>
+            <p><?php echo $info[$i]['work_descrip']?></p>
+            <hr>
+          </div>
+          <?php endfor;?>
+        </div>
+ 
+        <div class="w3-container w3-card w3-white">
+          <h2 class="w3-text-grey w3-padding-16"><i
+              class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>Образование</h2>
+          <div class="w3-container">
+            <h5 class="w3-opacity"><b>gb.ru</b></h5>
+            <h6 class="w3-text-teal"><i class="fa fa-calendar fa-fw w3-margin-right"></i>Forever</h6>
+            <p>Web Development! All I need to know in one place</p>
+            <hr>
+          </div>
+          <div class="w3-container">
+            классh5 <="w3-непрозрачность"><b>Лондонская школа бизнеса</b></h5>
+            классh6 <="w3-текст-бирюзовый"><i class="fa fa-календарь fa-fw w3-правое поле"></i>2013 - 2015</h6>
+            >p<Степень магистра</p>
+            <hr>
+          </div>
+          классdiv <="w3-контейнер">
+            классh5 <="w3-opacity"><b>Школа кодирования</b></h5>
+            классh6 <="w3-текст-бирюзовый"><i class="fa fa-календарь fa-fw w3-правое поле"></i>2010-2013</h6>
+            >p<Степень бакалавра</p><br>
+          </div>
+        </div>
+        <!-- Конец правой колонки -->
+      </div>
+      <!-- End Grid -->
+    </div>
+    <!-- Контейнер конечной страницы -->
+  </div>
+  <!-- Нижний колонтитул -->
+  класснижний колонтитул <="w3-контейнер w3-бирюзовый w3-центр w3-margin-top">
+    >p<Найдите меня в социальных сетях.</p>
+    классi <="fa fa-pinterest-p w3-hover-opacity"></i>
+    классi <="fa fa-twitter w3-hover-opacity"></i>
+    классi <="fa fa-linkedin w3-hover-opacity"></i>
+    <!-- Нижний колонтитул в конце -->
+  </нижний колонтитул>
+</тело>
+</html
